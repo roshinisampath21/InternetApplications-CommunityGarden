7882330a29ff05202325956d21689a30aabcc703
roshinisampath21 <roshini2199@gmail.com>
2024-07-22 19:41:02 -0400

    cookies

--- garden_app/middleware.py
+++ garden_app/middleware.py
@@ -6,17 +6,18 @@ class TrackUserVisitsMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
         current_date = datetime.now().date().isoformat()
-
+        user_history_path = '/user_history/'
         # Initialize session variables if not present
         if 'total_visits' not in request.session:
             request.session['total_visits'] = 0
             request.session['daily_visits'] = {}
 
         # Update total visits
-        request.session['total_visits'] += 1
+        if request.path == user_history_path:
+            request.session['total_visits'] += 1
 
-        # Update daily visit
-        if current_date in request.session['daily_visits']:
+        # Update daily visits
+        if current_date in request.session['daily_visits'] and request.path == user_history_path:
             request.session['daily_visits'][current_date] += 1
         else:
             request.session['daily_visits'][current_date] = 1
